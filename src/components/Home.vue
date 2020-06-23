1f4995230f78f6f1db58a353bf0dfcaaaee02d3d
Ade Mahendra <hendraadem@gmail.com>
2020-06-24 00:57:36 +0700

    add home component

---
+++ src/components/Home.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <Header/>
+        <Qurban/>
+        <Step/>
+        <Request/>
+    </div>
+</template>
+
+<script>
+    import '@/vendor.js'
+    import Header from '@/components/Header.vue'
+    import Qurban from '@/components/Qurban.vue'
+    import Step from '@/components/Step.vue'
+    import Request from '@/components/Request.vue'
+
+    export default {
+        name: 'Home',
+        components: {
+            Header, Qurban, Step, Request
+        }
+    }
+
+</script>
