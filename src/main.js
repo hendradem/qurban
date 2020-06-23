411603b612d1a0dfc405f4da5e6f7830c206611a
Ade Mahendra <hendraadem@gmail.com>
2020-06-23 23:44:22 +0700

    adding vue router

--- src/main.js
+++ src/main.js
@@ -1,9 +1,22 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueRouter from 'vue-router'
+import Header from './components/Header'
 import './vendor.js'
+
+
+Vue.use(VueRouter)
+
+const router = new VueRouter({
+  routes : [
+    {path: '/' , component: Header}
+  ],
+  mode : history
+})
 
 Vue.config.productionTip = false
 
 new Vue({
+  router,
   render: h => h(App),
 }).$mount('#app')
