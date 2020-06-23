e3030173c2a20b03bd23ed7854669061dc5a9c6a
Ade Mahendra <hendraadem@gmail.com>
2020-06-23 21:21:10 +0700

    change qurban card box shadow

--- src/components/Qurban.vue
+++ src/components/Qurban.vue
@@ -96,8 +96,8 @@ export default {
   .my-card {
     border: none;
     /* box-shadow: 1px 1px 5px 1px rgba(0, 0, 0, 0.04), 0 1px 2px 0 rgba(0, 0, 0, 0.06); */
-    /* box-shadow: 0 1px 3px 0 rgba(0, 0, 0, 0.2), 0 1px 2px 0 rgba(0, 0, 0, 0.06); */
-    box-shadow: 0 5px 12px -3px rgba(0, 0, 0, 0.1), 0 4px 6px -2px rgba(0, 0, 0, 0.05);
+    box-shadow: 0 1px 3px 0 rgba(0, 0, 0, 0.2), 0 1px 2px 0 rgba(0, 0, 0, 0.06);
+    /* box-shadow: 0 5px 12px -3px rgba(0, 0, 0, 0.1), 0 4px 6px -2px rgba(0, 0, 0, 0.05); */
     border-radius: 15px;
     height: auto;
     margin-bottom: 20px;
@@ -132,10 +132,6 @@ export default {
     background-color: #5a41c7;  
   }
 
-  /* tab */
-  .myTab {
-    
-  }
 
 
 
