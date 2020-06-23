60f5aa27ab822e1a3f7835d3a11a551551328639
Ade Mahendra <hendraadem@gmail.com>
2020-06-24 00:58:15 +0700

    removing router from main.js file and move it into routing file

--- src/main.js
+++ src/main.js
@@ -1,18 +1,7 @@
 import Vue from 'vue'
 import App from './App.vue'
-import VueRouter from 'vue-router'
-import Header from './components/Header'
-import './vendor.js'
-
-
-Vue.use(VueRouter)
-
-const router = new VueRouter({
-  routes : [
-    {path: '/' , component: Header}
-  ],
-  mode : history
-})
+import router from './router'
+import '@/vendor.js'
 
 Vue.config.productionTip = false
 
