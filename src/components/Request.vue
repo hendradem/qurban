fc5ddbb3d2f81728b8b19ac1972c4a532b353989
Ade Mahendra <hendraadem@gmail.com>
2020-06-23 20:42:48 +0700

    add request section

---
+++ src/components/Request.vue
@@ -0,0 +1,56 @@
+<template name="Request">
+    <div class="container">
+         <div class="qurbanHeading">
+              <h3 class="choiceHeading pt-5"> Info penyaluran daging qurban </h3>
+              <p>
+                  Silahkan klik menu dibawah ini untuk penyaluran daging qurban
+              </p>
+        </div>
+       <div class="row">
+           <div class="col-md-5">
+                <img class="img-fluid" src="../assets/images/request.png" />
+           </div>
+           <div class="col-md-7 d-flex justify-content-center">
+               
+                   <div class="row">
+                   <div class="card card-penyaluran bg-warning p-3 border-0">
+                        <div class="card-body">
+                            <h4 class="card-title"> <strong> Pengajuan penyaluran </strong> </h4>
+                            <p class="card-text">
+                                Klik disini untuk mengajukan proposal permintaan daging qurban
+                            </p>
+                            <a href="" class="btn btn-default btn-penyaluran btn-block mt-4"> Kirim proposal </a>
+                        </div>
+                    </div>
+                    <div class="card card-penyaluran card-p-2 p-3 border-0">
+                        <div class="card-body">
+                            <h4 class="card-title"> <strong> Pengajuan penyaluran </strong> </h4>
+                            <p class="card-text">
+                                Klik disini untuk mengajukan proposal permintaan daging qurban
+                            </p>
+                        </div>
+                    </div>
+                    </div>
+   
+           </div>
+       </div>
+    </div>
+</template>
+
+<style scoped>
+    .card-penyaluran {
+        width: 250px;
+        height: 300px;
+        margin: 10px;
+        margin-top: 100px;
+        border-radius: 10px;
+    }
+    .card-p-2 {
+        background-color: #673AB7;
+        color: #fff;
+    }
+    .btn-penyaluran {
+        background-color: #fff;
+        font-weight: 700;
+    }
+</style>
