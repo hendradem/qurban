d6d5648877d0dbef15c275e31462fffb4d39db45
Ade Mahendra <hendraadem@gmail.com>
2020-06-24 00:56:59 +0700

    adding bootstrap from App.vue file into vendor file

--- src/vendor.js
+++ src/vendor.js
@@ -1,10 +1,23 @@
 import Vue from 'vue'
+
+// bootstrap
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+// font awesome
 import {library} from '@fortawesome/fontawesome-svg-core'
 import {faUserSecret, faSpinner, faCheck, faArrowDown} from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import VueCountdown from '@chenfengyuan/vue-countdown';
 
-library.add(faUserSecret, faSpinner, faCheck, faArrowDown)
 
+// adding the library
+
+library.add(faUserSecret, faSpinner, faCheck, faArrowDown)
 Vue.component('fa-icon', FontAwesomeIcon);
 Vue.component(VueCountdown.name, VueCountdown);
+
+
+Vue.use(BootstrapVue)
+Vue.use(IconsPlugin)
