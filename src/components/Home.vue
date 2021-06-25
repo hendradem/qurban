8d4bea025203d80124b65555d4f0341aee0184a7
Ade Mahendra <hendraadem@gmail.com>
2021-06-25 23:21:05 +0700

    remove some component

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,23 +1,23 @@
 <template>
   <div>
     <Header />
-    <Whatsapporder />
-    <Request />
+    <!-- <Whatsapporder />
+    <Request /> -->
   </div>
 </template>
 
 <script>
 import "@/vendor.js";
 import Header from "@/components/Header.vue";
-import Whatsapporder from "@/components/Whatsapporder.vue";
-import Request from "@/components/Request.vue";
+// import Whatsapporder from "@/components/Whatsapporder.vue";
+// import Request from "@/components/Request.vue";
 
 export default {
   name: "Home",
   components: {
     Header,
-    Whatsapporder,
-    Request,
+    // Whatsapporder,
+    // Request,
   },
 };
 </script>
